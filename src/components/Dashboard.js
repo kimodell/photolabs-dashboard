40e3b018244b6432a852706bc8b4d10ed0bd6f56
Kim O'Dell <kimbo.odell@gmail.com>
2024-05-30 20:07:50 -0400

    Add API fetch requests

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,53 +3,77 @@ import React, { Component } from "react";
 import classnames from "classnames";
 import Loading from "./Loading";
 import Panel from "./Panel";
+import {
+  getTotalPhotos,
+  getTotalTopics,
+  getUserWithMostUploads,
+  getUserWithLeastUploads
+} from "helpers/selectors";
 
 const data = [
   {
     id: 1,
     label: "Total Photos",
-    value: 10
+    getValue: getTotalPhotos
   },
   {
     id: 2,
     label: "Total Topics",
-    value: 4
+    getValue: getTotalTopics
   },
   {
     id: 3,
     label: "User with the most uploads",
-    value: "Allison Saeng"
+    getValue: getUserWithMostUploads
   },
   {
     id: 4,
     label: "User with the least uploads",
-    value: "Lukas Souza"
+    getValue: getUserWithLeastUploads
   }
+
 ];
 
 class Dashboard extends Component {
   //set inital loading state to false 
   state = {
-    loading: false,
-    focused: null
+    loading: true,
+    focused: null,
+    photos: [],
+    topics: []
   };
 
-  //check if there is a svaved focus state when rendering app initally
+  //check if there is a saved focus state when rendering app initally
   componentDidMount() {
     const focused = JSON.parse(localStorage.getItem("focused"));
-    
+
     if (focused) {
       this.setState({ focused });
     }
+
+    const urlsPromise = [
+      "/api/photos",
+      "/api/topics",
+    ].map(url => fetch(url).then(response => response.json()));
+
+    Promise.all(urlsPromise)
+      .then(([photos, topics]) => {
+        this.setState({
+          loading: false,
+          photos: photos,
+          topics: topics
+        });
+      });
+
   }
-  
+
   //method to listen for changes to state
   componentDidUpdate(previousProps, previousState) {
     if (previousState.focused !== this.state.focused) {
       localStorage.setItem("focused", JSON.stringify(this.state.focused));
     }
   }
-  
+
   //function to set state of focused. If previous state is not null, return state to null, else expand panel for selected id
   selectPanel(id) {
     this.setState(previousState => ({
@@ -58,10 +82,12 @@ class Dashboard extends Component {
   }
 
   render() {
+
+    console.log(this.state);
     //redner dashboard focused css element when focused is truthy
     const dashboardClasses = classnames("dashboard", {
       "dashboard--focused": this.state.focused
-     });
+    });
     //display loading state when loading
     if (this.state.loading) {
       return <Loading />;
@@ -74,8 +100,8 @@ class Dashboard extends Component {
           <Panel
             key={panel.id}
             label={panel.label}
-            value={panel.value}
-            onSelect={(event) => this.selectPanel(panel.id)}
+            value={panel.getValue(this.state)}
+            onSelect={() => this.selectPanel(panel.id)}
           />
         ));
 
